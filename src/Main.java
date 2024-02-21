c6322d4548019c56436b3b37a2e4b57e33acc9ee
Beste Çeltek <besteceltek@gmail.com>
2024-02-21 23:29:27 +0300

    Added comments

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Defined the variables
         int a, b, c, result;
 
+        //Prompted three integer from the user
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter the first number: ");
         a = input.nextInt();
@@ -12,6 +14,7 @@ public class Main {
         System.out.print("Please enter the third number: ");
         c = input.nextInt();
 
+        //Calculated the a+b*c-b and printed the result
         result = a+b*c-b;
         System.out.println("The result is " + result);
     }
